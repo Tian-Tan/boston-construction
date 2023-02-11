5feae9b9442eac0e1b6a7361b03fcd500f6d4f6d
Anonymous <>
2023-02-11 09:48:13 -0500

    Stub

--- django_backend/boston_construction/apps.py
+++ django_backend/boston_construction/apps.py
@@ -4,3 +4,12 @@ from django.apps import AppConfig
 class BostonConstructionConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "boston_construction"
+<<<<<<< HEAD
+
+    def ready(self):
+        """
+        Django runs this code on app startup
+        """
+        # TODO get data
+=======
+>>>>>>> 40135c1 (re-organized)
