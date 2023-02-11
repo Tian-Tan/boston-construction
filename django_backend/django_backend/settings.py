1b041df4fcb01fc1490198297c51903d1715b6da
Anonymous <>
2023-02-11 10:48:00 -0500

    geodjango setup

--- django_backend/django_backend/settings.py
+++ django_backend/django_backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.urls",
     "boston_construction.apps.BostonConstructionConfig"
 ]
 
@@ -75,10 +76,11 @@ WSGI_APPLICATION = "django_backend.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'geodjango',
+        'USER': 'geo',
+    },
 }
 
 
