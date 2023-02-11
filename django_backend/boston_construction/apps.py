0272a9f1cf341707523458fc15a30ad2b7c85c33
Anonymous <>
2023-02-11 09:59:16 -0500

    data work

--- django_backend/boston_construction/apps.py
+++ django_backend/boston_construction/apps.py
@@ -1,7 +1,6 @@
 from django.apps import AppConfig
 
 # TODO is there a more generic way to refer to this, so that it's the one updated daily?
-DATA_URL = "https://data.boston.gov/api/3/action/datastore_search?offset={offset}&resource_id=36fcf981-e414-4891-93ea-f5905cec46fc"
 
 
 class BostonConstructionConfig(AppConfig):
@@ -12,4 +11,12 @@ class BostonConstructionConfig(AppConfig):
         """
         Django runs this code on app startup
         """
+        offset = 0
+        response = requests.get(f"https://data.boston.gov/api/3/action/datastore_search?offset={offset}&resource_id=36fcf981-e414-4891-93ea-f5905cec46fc")
+        data_txt = response.text # TODO do we need to index a particular json point?
+        while response.status_code == 200:
+            offset += 100 #loltacular code
+            response = requests.get(f"https://data.boston.gov/api/3/action/datastore_search?offset={offset}&resource_id=36fcf981-e414-4891-93ea-f5905cec46fc")
+            data_txt += response.text
+
         # TODO get data
