f83c78f8785f369a5bc1443b88dd4c61fdff785b
Anonymous <>
2023-02-11 15:43:44 -0500

    major fix

--- django_backend/boston_construction/tasks.py
+++ django_backend/boston_construction/tasks.py
@@ -25,3 +25,26 @@ def send_emails():
             "MessageStream": "outbound"
         }
         response = requests.post("https://api.postmarkapp.com/email", headers=headers, json=data)
+
+def get_data():
+    # TODO is there a more generic way to refer to this, so that it's the one updated daily?
+    response = requests.get(f"https://data.boston.gov/api/3/action/datastore_search?offset=0&resource_id=36fcf981-e414-4891-93ea-f5905cec46fc")
+    data_json = response.json()
+    total = data_json["result"]["total"]
+    print(f"Total is {total}")
+    records = []
+    while len(records) != total:
+        response = requests.get(f"https://data.boston.gov/api/3/action/datastore_search?offset={len(records)}&resource_id=36fcf981-e414-4891-93ea-f5905cec46fc")
+        records += response.json()["result"]["records"]
+        print(f"Got {len(records)} objects")
+
+    # Creates models for every record, gets their location, and save them into the database
+    for record in records:
+        work = ConstructionRecord(_id=record['_id'], neighborhood=record['Neighborhood'], street=record['Street'], address_1=record['Address_1'],
+                address_2=record['Address_2'], intersection=record['Intersection'], start=record['From'], to=record['To'], permittee=record['Permittee'],
+                contractor=record['Contractor'], permit=record['Permit'], project_category=record['Project_Category'], construction_notes=record['Construction_Notes'],
+                work_schedule=record['Work_Schedule'], expiration_date=record['ExpirationDate'], estimated_completion_date=record['Estimated_Completion_Date'],
+                roadway_plates_in_use=record['Roadway_Plates_In_Use'], sidewalk_plates_in_use=record['Sidewalk_Plates_In_Use'], status=record['Status'],
+                trench_length=record['Trench_Length'], contact_number=record['Contact_Number'], number_of_works=record['NumberOfWorkZones'],
+                district=record['District'], lat=0.0, _long=0.0)
+        work.save()
