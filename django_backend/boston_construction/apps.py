3691a29ce294cd7df0426d1f4c7c167ac9558aee
Anonymous <>
2023-02-11 10:05:48 -0500

    work on get data

--- django_backend/boston_construction/apps.py
+++ django_backend/boston_construction/apps.py
@@ -14,9 +14,13 @@ class BostonConstructionConfig(AppConfig):
         offset = 0
         response = requests.get(f"https://data.boston.gov/api/3/action/datastore_search?offset={offset}&resource_id=36fcf981-e414-4891-93ea-f5905cec46fc")
         data_txt = response.text # TODO do we need to index a particular json point?
+        data = response.json()["result"]["records"]
         while response.status_code == 200:
             offset += 100 #loltacular code
             response = requests.get(f"https://data.boston.gov/api/3/action/datastore_search?offset={offset}&resource_id=36fcf981-e414-4891-93ea-f5905cec46fc")
             data_txt += response.text
+            tmp = response.json()["result"]["records"]
+            print(tmp)
+
 
         # TODO get data
